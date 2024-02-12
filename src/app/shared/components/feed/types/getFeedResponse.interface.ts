47b36ba359df5fae1b91beb6958ebdede175b019
Popa Serban Alexandru <serban.popa@outlook.com>
2024-02-12 20:47:34 +0200

    Refactor code for improved performance

---
+++ src/app/shared/components/feed/types/getFeedResponse.interface.ts
@@ -0,0 +1,7 @@
+import { ArticleInterface } from "../../../types/articles.interface";
+
+export interface GetFeedResponseInterface {
+    articles:ArticleInterface[];
+    articlesCount: number;
+
+}
